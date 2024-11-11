3516a399b7e3cf2ac7ebcb69842301b1a6b8c63d
Solomonadams <156305900+Solomonadams@users.noreply.github.com>
2024-11-11 06:42:01 +0100

    Update app.py

--- code/app.py
+++ code/app.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-#import tensorflow as tf
-#from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, mean_squared_error
